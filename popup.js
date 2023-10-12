2d06a23a67aa50092a090e3f6f86492bd403731d
Baris Kiral <bariskiral07@gmail.com>
2023-10-12 15:09:32 +0300

    Initial

---
+++ popup.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var button = document.getElementById("pickerButton");
+  button.addEventListener("click", function () {
+    // Add your button click functionality here
+    alert("Button Clicked!");
+  });
+});
